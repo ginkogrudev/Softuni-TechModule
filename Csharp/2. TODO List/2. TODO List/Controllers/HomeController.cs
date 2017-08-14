aa1c775e4a60286ce8a5d5125ce3d33b1f522e83
ginkogrudev <ginko404@gmail.com>
2017-08-15 02:14:16 +0300

    Added TODO List Task

---
+++ Csharp/2. TODO List/2. TODO List/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using _2.TODO_List.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2.TODO_List.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var db = new TaskDbContext())
+            {
+                var tasks = db.Tasks.ToList();
+
+                return View(tasks);
+            }
+        }
+    }
+}
