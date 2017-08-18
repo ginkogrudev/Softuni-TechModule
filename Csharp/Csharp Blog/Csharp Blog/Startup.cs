083d4d76e8a3153aaf1b493a4ee417c24ab6e9c3
ginkogrudev <ginko404@gmail.com>
2017-08-18 13:09:19 +0300

    Added blog

---
+++ Csharp/Csharp Blog/Csharp Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Csharp_Blog.Startup))]
+namespace Csharp_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Csharp/Exam test/Exam test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var y = int.Parse(Console.ReadLine());
+            var c = n;
+            var count = 0;
+            while (n >= m)
+            {
+                n = n - m;
+                count++;
+                if (c/2 == n)
+                {
+                    n = n / y;
+                }
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(count);
+        }
+    }
+}
